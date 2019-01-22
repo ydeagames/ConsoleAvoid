7c0f453f6246007b9c7ee12387b41facbf95dcac
YdeaGames <ydeagames@gmail.com>
2019-01-22 10:12:58 +0900

    環境

--- Libraries/GameLibrary/Shape.h
+++ Libraries/GameLibrary/Shape.h
@@ -1,5 +1,7 @@
 #pragma once
 
+class Transform;
+
 // <‰¡ˆÊ’uŠÖŒW>
 enum class HorizontalSide
 {
---
+++ Libraries/Library.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "Utilities\MemoryLeakDetector.h"
+#include "BaseLibrary\BaseLibrary.h"
+#include "CXLibrary\CXLibrary.h"
+#include "GameLibrary\GameLibrary.h"
